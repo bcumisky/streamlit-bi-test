3cf569556abf8409202fd9fed5435a251ee4323d
bcumisky <bcumisky@gmail.com>
2022-11-21 23:51:42 -0500

    Update 04_Plotly Scatter.py

--- pages/04_Plotly Scatter.py
+++ pages/04_Plotly Scatter.py
@@ -23,9 +23,9 @@ with st.spinner('Updating Report...'):
     hosp_df = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Hospitals')
     hosp = st.selectbox('Choose Hospital', hosp_df, help = 'Filter report to show only one hospital')
 
-    # Number of Completed Handovers by Hour
     g1, g2, g3 = st.columns((1,1,1))
     
+    # Patient Satisfaction by Patient Hospital
     fgdf = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Graph')
     
     fgdf = fgdf[fgdf['Hospital Attended']==hosp] 
@@ -34,12 +34,11 @@ with st.spinner('Updating Report...'):
     
     fig.update_traces(marker_color='#264653')
     
-    fig.update_layout(title_text="Number of Completed Handovers by Hour",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
+    fig.update_layout(title_text="Patient Satisfaction by Hospital",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
     g1.plotly_chart(fig, use_container_width=True) 
     
-    # Predicted Number of Arrivals
-    
+    # Patient Satisfaction by Patient Type
     fcst = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Forecast')
     
     fcst = fcst[fcst['Hospital Attended']==hosp]
@@ -48,16 +47,15 @@ with st.spinner('Updating Report...'):
     
     fig.update_traces(marker_color='#7A9E9F')
     
-    fig.update_layout(title_text="Predicted Number of Arrivals",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
+    fig.update_layout(title_text="Patient Satisfaction by Patient Type",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
     
     g2.plotly_chart(fig, use_container_width=True)  
     
-    # Average Completed Handover Duration by hour
-
+    # Patient Satisfaction by Service Line
     fig = px.bar(fgdf, x = 'Arrived Destination Resolved', y='Average Duration',color = "Average Duration", template = 'seaborn', color_continuous_scale=px.colors.diverging.Temps)
     
     fig.add_scatter(x=fgdf['Arrived Destination Resolved'], y=fgdf['Target'], mode='lines', line=dict(color="black"), name='Target')
     
-    fig.update_layout(title_text="Average Completed Handover Duration by hour",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None, legend=dict(orientation="h",yanchor="bottom",y=0.9,xanchor="right",x=0.99))
+    fig.update_layout(title_text="Patient Satisfaction by Service Line",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None, legend=dict(orientation="h",yanchor="bottom",y=0.9,xanchor="right",x=0.99))
     
     g3.plotly_chart(fig, use_container_width=True) 
