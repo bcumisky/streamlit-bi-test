a1cdadde964df571d29d10466ce2c5bd20089ff9
bcumisky <bcumisky@gmail.com>
2022-11-21 23:35:38 -0500

    Update 04_Plotly Scatter.py

--- pages/04_Plotly Scatter.py
+++ pages/04_Plotly Scatter.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 import plotly_express as px
 
 df = st.session_state['df']
@@ -12,3 +14,50 @@ chart_color = st.color_picker('Select a plot color')
 plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
 plot.update_traces(marker=dict(color=chart_color))
 st.plotly_chart(plot, use_container_width=True)
+
+
+## Data
+with st.spinner('Updating Report...'):
+    
+    #Metrics setting and rendering
+    hosp_df = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Hospitals')
+    hosp = st.selectbox('Choose Hospital', hosp_df, help = 'Filter report to show only one hospital')
+
+    # Number of Completed Handovers by Hour
+    g1, g2, g3 = st.columns((1,1,1))
+    
+    fgdf = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Graph')
+    
+    fgdf = fgdf[fgdf['Hospital Attended']==hosp] 
+    
+    fig = px.bar(fgdf, x = 'Arrived Destination Resolved', y='Number of Handovers', template = 'seaborn')
+    
+    fig.update_traces(marker_color='#264653')
+    
+    fig.update_layout(title_text="Number of Completed Handovers by Hour",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
+    
+    g1.plotly_chart(fig, use_container_width=True) 
+    
+    # Predicted Number of Arrivals
+    
+    fcst = pd.read_excel('data/DataforMock.xlsx',sheet_name = 'Forecast')
+    
+    fcst = fcst[fcst['Hospital Attended']==hosp]
+    
+    fig = px.bar(fcst, x = 'Arrived Destination Resolved', y='y', template = 'seaborn')
+    
+    fig.update_traces(marker_color='#7A9E9F')
+    
+    fig.update_layout(title_text="Predicted Number of Arrivals",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None)
+    
+    g2.plotly_chart(fig, use_container_width=True)  
+    
+    # Average Completed Handover Duration by hour
+
+    fig = px.bar(fgdf, x = 'Arrived Destination Resolved', y='Average Duration',color = "Average Duration", template = 'seaborn', color_continuous_scale=px.colors.diverging.Temps)
+    
+    fig.add_scatter(x=fgdf['Arrived Destination Resolved'], y=fgdf['Target'], mode='lines', line=dict(color="black"), name='Target')
+    
+    fig.update_layout(title_text="Average Completed Handover Duration by hour",title_x=0,margin= dict(l=0,r=10,b=10,t=30), yaxis_title=None, xaxis_title=None, legend=dict(orientation="h",yanchor="bottom",y=0.9,xanchor="right",x=0.99))
+    
+    g3.plotly_chart(fig, use_container_width=True) 
