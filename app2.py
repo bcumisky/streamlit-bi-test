430d9f5421f1126ba1c8018d11393af8ddf5b26a
bcumisky <bcumisky@gmail.com>
2022-11-21 20:01:54 -0500

    Delete app2.py

--- app2.py
+++
@@ -1,5 +0,0 @@
-import pdfkit
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
-from datetime import date
-import streamlit as st
-from streamlit.components.v1 import iframe
